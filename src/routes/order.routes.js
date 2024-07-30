95d5a360f8579810ce51ec1b99d40c336924975d
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-07-30 15:17:01 +0300

    integrates the auth middleware to protect routes

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { createOrder, deleteOrder, getOrder, getOrderbyId, updateOrder} from '../controller/order.controller.js';
+import { protect } from '../middleware/auth.js';
 
 
 const router = express.Router();
 
-router.post('/create-order', createOrder);
-router.get('/orders', getOrder);
-router.get('/orders/:id', getOrderbyId);
-router.put('/orders/:id',updateOrder);
-router.delete('/orders/:id', deleteOrder)
+router.post('/create-order',protect, createOrder);
+router.get('/orders',protect, getOrder);
+router.get('/orders/:id',protect,getOrderbyId);
+router.put('/orders/:id',protect,updateOrder);
+router.delete('/orders/:id',protect, deleteOrder)
 
 export default router
